28d1a157c4c6b28bed3e1163461fdf6e39930b6a
hameed-afsar-km <hameedafsar2006@gmail.com>
2026-02-15 23:59:24 +0530

    Form DropDown UseState

--- Learning/1/src/form.jsx
+++ Learning/1/src/form.jsx
@@ -4,6 +4,7 @@ function Form() {
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
     const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("None");
 
     return (<form>
         <input type="text" placeholder="Enter Your Name" value={name} onChange={(e) => setName(e.target.value)}></input>
@@ -12,6 +13,13 @@ function Form() {
         <p>Your Age is {age}</p>
         <textarea placeholder="Enter your comments..." onChange={(e) => setComment(e.target.value)}></textarea>
         <p>Comment: {comment}</p>
+        <select onChange={(e) => setPayment(e.target.value)}>
+            <option value="" selected>Select an Option</option>
+            <option value="Gpay">GPay</option>
+            <option value="PayTM">PayTM</option>
+            <option value="UPI">UPI</option>
+        </select>
+        <p>You Selected {payment}</p>
     </form>);
 }
 
